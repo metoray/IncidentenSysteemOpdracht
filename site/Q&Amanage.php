86bb0fa0d9c4acdd8ef73fb38e0ead2d4015bfef
RikJanS <r.j.schuringa@st.hanze.nl>
2015-06-19 11:24:27 +0200

    Q&A beheren is klaar
    Q&A beheren afgemaakt. De verwijzingen van de href moeten misschien nog
    worden aangepast.

--- site/Q&Amanage.php
+++ site/Q&Amanage.php
@@ -11,7 +11,7 @@
 
 
 	//Query voor de gegevens van de vraag tabel op te halen.
-	$query = "SELECT id, tekst ";
+	$query = "SELECT * ";
 	$query .= "FROM vraag";
 	
 	//Zet de resultaten van query vraag tabel in variabel.
@@ -24,7 +24,7 @@
 
 
 	//Query voor de gegevens van de vraag antwoord op te halen.
-	$query = "SELECT vraag_id, tekst ";
+	$query = "SELECT * ";
 	$query .= "FROM antwoord";
 
 	//Zet de resultaten van query antwoord tabel in variabel.
@@ -32,16 +32,16 @@
 
 	//Zet de resultaten van de query in een associative array.
 	while ($antwoorden_query = mysql_fetch_assoc($result)) {
-		$antwoorden[] = array('Vraag_Id' => intval($antwoorden_query["vraag_id"]), 'Antwoord_Tekst' => $antwoorden_query["tekst"],);
+		$antwoorden[] = array('Antwoord_Id' => intval($antwoorden_query["id"]), 'Vraag_Id' => intval($antwoorden_query["vraag_id"]), 'Antwoord_Tekst' => $antwoorden_query["tekst"], 'VervolgvraagId' => intval($antwoorden_query["vervolg_vraag_id"]), 'DefaultTicketId' => intval($antwoorden_query["default_ticket_id"]));
 	}
 
 
 	//Tabel wordt gemaakt met kolomnamen.
 	echo "<table border=1><tr><td><b>Vraag ID</b></td><td><b>Vraag</b></td><td><b>Antwoorden</b></td>";
 
-	//Loop om alle vragen en aantwoorden te tonen in de tabel.
+	//Loop om alle vragen en aantwoorden te tonen in de tabel. Ook kan er worden geklikt op de vragen en antwoorden. Er wordt dan naar de pagina Q&A verwezen met een GET met de waarde van id.
 	foreach ($vragen as $i) {
-		echo "<tr><td>". $i['Vraag_Id']. "</td><td>". $i['Vraag_Tekst']. "</td>";
+		echo "<tr><td>". "<a href=\"Q&A.php?vraagid=". $i['Vraag_Id']. "\">". $i['Vraag_Id']. "</a>". "</td><td>". "<a href=\"Q&A.php?vraagid=". $i['Vraag_Id']. "\">". $i['Vraag_Tekst']. "</a>". "</td>";
 		
 		$counter = 0;
 
@@ -53,7 +53,7 @@
 					echo "<td></td><td></td>";
 				}
 				
-				echo "<td>". $j['Antwoord_Tekst']. "</td></tr>";
+				echo "<td>". "<a href=\"Q&A.php?antwoordid=". $j['Antwoord_Id']. "\">". $j['Antwoord_Tekst']. "</a>". "</td></tr>";
 				$counter ++;
 			}
 
