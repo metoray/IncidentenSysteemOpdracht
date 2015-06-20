811b82f9f0b57641af8ace2786a1175416e2f9e9
metoray <metoray@gmail.com>
2015-06-20 14:36:31 +0200

    Title now displays in index.php

--- site/index.php
+++ site/index.php
@@ -25,7 +25,7 @@ if($currentPage){
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php $title; ?></title>
+	<title><?php echo $title; ?></title>
 	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
