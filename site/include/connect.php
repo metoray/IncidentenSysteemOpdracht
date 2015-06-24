56f4d780684a03bb6454b18817dc235dc5f4719e
jipjop13 <h.venema@st.hanze.nl>
2015-06-24 15:33:01 +0200

    implement MYSQLI

--- site/include/connect.php
+++ site/include/connect.php
@@ -1,7 +1,16 @@
 <?php
-mysql_connect("localhost", "rick_groep1", "Welkom01");
-$db_selected = mysql_select_db("rick_hondsrug");
+//mysql_connect("localhost", "rick_groep1", "Welkom01");
+//$db_selected = mysql_select_db("rick_hondsrug");
+
+$dbhost = "localhost";  
+$dbuser = "rick_groep1";  
+$dbpass = "Welkom01";  
+$dbname = "rick_hondsrug";  
+
+$con=mysqli_connect("$dbhost","$dbuser","$dbpass","$dbname");
+
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+
 ?>
