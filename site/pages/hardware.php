aa2f491a520d5b95e38e43c41709501c1c62be60
metaladdict6 <r.r.c.ziengs@st.hanze.nl>
2015-06-25 00:17:43 +0200

    ja

--- site/pages/hardware.php
+++ site/pages/hardware.php
@@ -9,7 +9,7 @@
 	$query .= "JOIN soort_hardware ON (hardwarecomponenten.soort_id = soort_hardware.soort_h_id) ";
 	$query .= "JOIN merken ON (hardwarecomponenten.merk_id = merken.merk_id) ";
 	$query .= "JOIN locaties ON (hardwarecomponenten.locatie_id = locaties.locatie_id)";
-	
+	echo $query;
 	
 	//Zet de resultaten van query variabel zetten.
 	$result = mysqli_query($con,$query);
@@ -21,10 +21,10 @@
 
 
 	//Tabel wordt gemaakt met kolomnamen.
-	echo "<table border=1><tr><td><b>Identificatiecode</b></td><td><b>Soort hardware</b></td><td><b>Locatie</b></td><td><b>Merk</b></td></td><td><b>Leverancier</b></td></td><td><b>Aanschaf datum </b></td></td></tr>";
+	echo '<table class="table table-striped"><thead><tr><th>Identificatiecode</th><th>Soort hardware</th><th>Locatie</th><th>Merk</th></th><th>Leverancier</th></th><th>Aanschaf datum </th></th></tr></thead>';
 
 	foreach ($Hardware as $i) {
-		echo "<tr><td><a href=\"/cmdb/hardware/installation?identification_code=". $i['Identificatiecode']. "\">". $i['Identificatiecode']. "</a></td><td>". $i['SoortHardware']. "</td><td>". $i['Locatie']. "</td><td>". $i['MerkNaam']. "</td></td><td>". $i['LeverancierNaam']. "</td></td><td>". $i['JaarVanAanschaf']. "</td></td></tr>";
+		echo "<tr><td><a href=/cmdb/hardware/installation?identification_code='".$i['Identificatiecode']."'\">". $i['Identificatiecode']. "</a></td><td>". $i['SoortHardware']. "</td><td>". $i['Locatie']. "</td><td>". $i['MerkNaam']. "</td></td><td>". $i['LeverancierNaam']. "</td></td><td>". $i['JaarVanAanschaf']. "</td></td></tr>";
 	}
 	echo "</table>";
 ?>
