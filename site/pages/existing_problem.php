61bfd721ae385a5958b77a945f24ddae47f6a372
metaladdict6 <r.r.c.ziengs@st.hanze.nl>
2015-06-25 09:33:51 +0200

    update

--- site/pages/existing_problem.php
+++ site/pages/existing_problem.php
@@ -109,7 +109,7 @@ $practitoner_result = mysqli_query($con, $search_practitoner) or die(mysql_error
 		{
 		 	echo "<tr>";
 		 		echo "<td>";
-						echo "<a href=/incidents/existing?incidentid=".$inc_rows[0]."> ".$inc_rows[0]."	</a>";
+						echo "<a href=/incidents/open?incidentid=".$inc_rows[0]."> ".$inc_rows[0]."	</a>";
 				echo "</td>";
 		 		echo "<td>";
 					echo $inc_rows[1] ;
