b94faa77b0119651f310081757e998b3b8dae7c9
metoray <metoray@gmail.com>
2015-07-01 13:01:29 +0200

    Added bootstrap to new_problem.php.

--- site/pages/new_problem.php
+++ site/pages/new_problem.php
@@ -5,39 +5,45 @@ $practitoner_result = mysqli_query($con,$search_practitoner) or die(mysql_error(
 ?>
 <html>
 	<body>
-		<form name="new_problem" action= "/process/robert" method="POST" id="new_problem">
-		<br />
-		Behandelaar:<select name="practitioner">
-		
-		<?php
-			while($practitoner_row = mysqli_fetch_array($practitoner_result))
-			{
-				?>
-				
-					<option value = <?php echo $practitoner_row[0] ?> >	<?php echo $practitoner_row[1] ?> </option>
-				<?php
-			}
-			$search_statussen_probleem = "select * from statussen_probleem";
-			$search_statussen_probleem_result = mysqli_query($con,$search_statussen_probleem) or die(mysql_error());
-		?>
-		</select>
-		<select name ="status">
-		<?php
-			while($search_statussen_probleem_row = mysqli_fetch_array($search_statussen_probleem_result))
-			{
-				?>
-				
-					<option value = <?php echo $search_statussen_probleem_row[0] ?> >	<?php echo $search_statussen_probleem_row[1] ?> </option>
-				<?php
-			}
-		?>
-		</select>
-		<br />
-		<br />
-	
-		<textarea rows="25" cols="150" name="description" form="new_problem"  >Schrijf een beschrijving van het probleem hier</textarea>
-		<br />
-		<input type="submit" name="new_problem" value = "Ga verder">
+		<form class="form-horizontal" name="new_problem" action= "/process/robert" method="POST" id="new_problem">
+			<div class="form-group">
+				<label for="employee" class="col-sm-2 control-label">Behandelaar:</label>
+				<div class="col-sm-5">
+					<select class="form-control" name="practitioner" id="employee">
+					
+					<?php
+						while($practitoner_row = mysqli_fetch_array($practitoner_result))
+						{
+							?>
+							
+								<option value = <?php echo $practitoner_row[0] ?> >	<?php echo $practitoner_row[1] ?> </option>
+							<?php
+						}
+						$search_statussen_probleem = "select * from statussen_probleem";
+						$search_statussen_probleem_result = mysqli_query($con,$search_statussen_probleem) or die(mysql_error());
+					?>
+					</select>
+				</div>
+				<div class="col-sm-5">
+					<select class="form-control" name ="status">
+					<?php
+						while($search_statussen_probleem_row = mysqli_fetch_array($search_statussen_probleem_result))
+						{
+							?>
+							
+								<option value = <?php echo $search_statussen_probleem_row[0] ?> >	<?php echo $search_statussen_probleem_row[1] ?> </option>
+							<?php
+						}
+					?>
+					</select>
+				</div>
+			</div>
+			<div class="form-group">
+				<textarea class="form-control" rows="25" cols="150" name="description" form="new_problem"  >Schrijf een beschrijving van het probleem hier</textarea>
+			</div>
+			<div class="form-group">
+				<input class="btn btn-primary" type="submit" name="new_problem" value = "Ga verder">
+			</div>
 		</form>
 
 	</body>
